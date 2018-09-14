44f6da2bf4fe216fc35c14e97af75f20a5922f7f
Erica Lieske <42978205+elie1901@users.noreply.github.com>
2018-09-14 12:36:10 -0500

    Add - MediaLib

---
+++ MediaLib/MediaLib.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class MediaLib here.
+ *
+ * @author (Erica)
+ * @version (9/10/18)
+ */
+public class MediaLib
+{
+      public static void main()
+    {
+        System.out.println("Welcome to your Media Library");
+    }
+
+}
---
+++ MediaLib/Song.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class Song here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Song
+{
+    // instance variables - replace the example below with your own
+    private int rating;
+    private String title;
+   
+
+    /**
+     * Constructor for objects of class Song
+     */
+    public Song()
+    {
+        // initialise instance variables
+        title = "";
+    }
+
+}
